49e02159298e4721756d53aaed26600a3bd5f69b
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2026-08-09 14:19:05 -0700

    Intergrate doc splitter in main RAG project

--- RAG_Project/main.py
+++ RAG_Project/main.py
@@ -1,12 +1,20 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
-data = TextLoader("RAG_Project/document-loaders/notes.txt")
-docs = data.load()
+
+loader = PyPDFLoader("RAG_Project/document-loaders/Covariance vs Correlation.pdf")
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+chunks = text_splitter.split_documents(docs)
+
+print("Length:", len(chunks))
+print("Content:", chunks[0])
 
 template = ChatPromptTemplate.from_messages(
     [("system", "You are the AI that summarize the content"), ("human", "{data}")]
