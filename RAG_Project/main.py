b7a460bc880ebd6848c058423b899b62bb9c86c0
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2026-08-15 11:38:39 -0700

    Implement Chroma in project

--- RAG_Project/main.py
+++ RAG_Project/main.py
@@ -1,20 +1,8 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
-from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
-
-
-loader = PyPDFLoader("RAG_Project/document-loaders/Covariance vs Correlation.pdf")
-docs = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-chunks = text_splitter.split_documents(docs)
-
-print("Length:", len(chunks))
-print("Content:", chunks[0])
 
 template = ChatPromptTemplate.from_messages(
     [("system", "You are the AI that summarize the content"), ("human", "{data}")]
