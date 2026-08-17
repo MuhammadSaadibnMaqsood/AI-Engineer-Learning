e9d6c8c3aee2eaa0ec2fcd264ea0b7e29873e6b6
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2026-08-17 13:29:59 -0700

    Working on main project

--- RAG_Project/main.py
+++ RAG_Project/main.py
@@ -1,14 +1,40 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_mistralai import MistralAIEmbeddings
+from langchain_community import Chroma
 
 load_dotenv()
 
-template = ChatPromptTemplate.from_messages(
-    [("system", "You are the AI that summarize the content"), ("human", "{data}")]
+model = ChatMistralAI(model="mistral-small-2506")
+embedding_model = MistralAIEmbeddings()
+vectorstore = Chroma(persist_directory="chroma_db", embedding_function=embedding_model)
+retriever = vectorstore.as_retriever(
+    search_type="mmr", search_kwargs={"k": 4, "fetch_k": 10, "lambda_mult": 0.5}
 )
-model = ChatMistralAI(model="mistral-small-2506", temperature=0.3)
 
-prompt = template.format_messages(data=docs[0].page_content)
-result = model.invoke(prompt)
-print(result.content)
+# prompt template
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """You are a helpful AI assistant.
+
+Use ONLY the provided context to answer the question.
+
+If the answer is not present in the context,
+say: "I could not find the answer in the document."
+""",
+        ),
+        (
+            "human",
+            """Context:
+{context}
+
+Question:
+{question}
+""",
+        ),
+    ]
+)
