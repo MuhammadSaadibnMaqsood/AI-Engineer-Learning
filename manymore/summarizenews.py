526297135898cae1d7dcb81bbfa74d79e2713e2f
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2026-08-24 11:40:07 -0700

    Practice tavily

---
+++ manymore/summarizenews.py
@@ -0,0 +1,40 @@
+import getpass
+import os
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_mistralai import ChatMistralAI
+from langchain_tavily import TavilySearch
+
+load_dotenv()
+
+if not os.environ.get("TAVILY_API_KEY"):
+    os.environ["TAVILY_API_KEY"] = getpass.getpass("Tavily API key:\n")
+
+tool = TavilySearch(
+    max_results=5,
+    topic="general",
+)
+
+response = tool.invoke({"query": "What happened at the last wimbledon"})
+
+content = [doc["content"] for doc in response["results"]]
+
+llm = ChatMistralAI(model="mistral-small-2506")
+
+prompt = ChatPromptTemplate(
+    [
+        (
+            "system",
+            "You are an AI assistant that summarizes content into maximum 5 bullet points.",
+        ),
+        ("human", "summarize this content {content} into 5 bullet points"),
+    ]
+)
+
+parser = StrOutputParser()
+chain = prompt | llm | parser
+
+result = chain.invoke({"content": content})
+
+print("\n", result)
