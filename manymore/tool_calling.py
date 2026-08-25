56c6b575c0200903a6b3cc87b1579dbb7c5075aa
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2026-08-25 13:07:26 -0700

    Practice tool calling

--- manymore/tool_calling.py
+++ manymore/tool_calling.py
@@ -12,8 +12,9 @@ def get_text_length(text: str) -> int:
     """Return a number of character in a given text"""
     return len(text)
 
+
 @tool
-def greeting(name:str) -> str:
+def greeting(name: str) -> str:
     """This is a simple Greeting tool"""
     return f"Hello {name}! Welcome to the GenAI course"
 
@@ -21,6 +22,16 @@ def greeting(name:str) -> str:
 llm = ChatMistralAI(model="mistral-small-2506")
 
 # tool binding
-
 llm_with_tool = llm.bind_tools([get_text_length])
 
+result = llm_with_tool.invoke(
+    "Return the number of character in a given text: 'Hello how are you ? '"
+)
+
+if result.tool_calls:
+    tool_call = result.tool_calls[0]
+    tool_result = get_text_length.invoke(tool_call["args"])
+
+    final_response = llm.invoke(f"The length of the text is {tool_result}")
+
+    print(final_response)
