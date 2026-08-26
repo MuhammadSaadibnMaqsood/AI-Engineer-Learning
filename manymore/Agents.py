63a0fd60ebc4a4d6cf384ce0fd0ffc551a55f3af
MuhammadSaadibnMaqsood <muhammadsaady0100@gmail.com>
2026-08-26 06:21:47 -0700

    Complete get wheather tool

--- manymore/Agents.py
+++ manymore/Agents.py
@@ -10,7 +10,23 @@ from langchain_core.messages import HumanMessage, ToolMessage
 from tavily import TavilyClient
 
 API_KEY = os.getenv("OPENWHEATHER_API_KEY")
+
+
 @tool
 def get_wheather(city: str) -> str:
     """Get current wheather of a city"""
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city},IN&appid={API_KEY}&units=metric"
+    response = requests.get(url)  # Fixed: changed `response.get` to `requests.get`
+    data = response.json()
+    if (
+        str(data.get("cod")) != "200"
+    ):  # Fixed: changed 'code' to OpenWeather API's 'cod'
+        return f"Error: {data.get('message', 'could not fetch wheather')}"
+
+    temp = data["main"]["temp"]
+    desc = data["weather"][0]["description"]  # Fixed: changed 'wheather' to 'weather'
+
+    return f"Wheather in {city}: {desc}, {temp}C "
+
+
+tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
